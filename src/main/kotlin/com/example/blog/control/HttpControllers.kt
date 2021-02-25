5035a03f4d3c39d38b2e41d726c58b1f8b3443d5
Erik Aschenbrenner <ashburnere@gmail.com>
2021-02-25 14:52:48 +0100

    Swagger UI hinzufügen

--- src/main/kotlin/com/example/blog/control/HttpControllers.kt
+++ src/main/kotlin/com/example/blog/control/HttpControllers.kt
@@ -1,5 +1,9 @@
-package com.example.blog
+package com.example.blog.control
 
+import com.example.blog.Article
+import com.example.blog.ArticleRepository
+import com.example.blog.User
+import com.example.blog.UserRepository
 import org.springframework.http.HttpStatus.*
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.server.ResponseStatusException
@@ -15,6 +19,14 @@ class ArticleController(private val repository: ArticleRepository) {
 	fun findOne(@PathVariable slug: String) =
 			repository.findBySlug(slug) ?: throw ResponseStatusException(NOT_FOUND, "This article does not exist")
 
+	@PostMapping("/")
+	fun addArticle(@RequestBody article: Article) = repository.save(article)
+
+	@DeleteMapping("/{id}")
+	fun deleteArticleById(@PathVariable id: Long) {
+		repository.deleteById(id)
+	}
+
 }
 
 @RestController
@@ -26,4 +38,10 @@ class UserController(private val repository: UserRepository) {
 
 	@GetMapping("/{login}")
 	fun findOne(@PathVariable login: String) = repository.findByLogin(login) ?: throw ResponseStatusException(NOT_FOUND, "This user does not exist")
+
+	@PostMapping("/")
+	fun addUser(@RequestBody user: User) = repository.save(user)
+
+	@DeleteMapping("/{id}")
+	fun deleteUserById(@PathVariable id: Long) = repository.deleteById(id)
 }
